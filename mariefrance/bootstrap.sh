01922029686c7dd0d24641ee50af86e1f510b9f1
Sean Graham <github@sean-graham.com>
2024-12-23 17:00:19 -0500

    added a bunch of bootstrap, including unifi+ports

--- mariefrance/bootstrap.sh
+++ mariefrance/bootstrap.sh
@@ -6,12 +6,10 @@
 echo "https://cdn.openbsd.org/pub/OpenBSD/" > /etc/installurl
 
 export PKG_PATH=https://cdn.openbsd.org/%m
-pkg_add zsh
-pkg_add python
-pkg_add nut
-pkg_add git
-pkg_add vim
-pkg_add tailscale
+pkg_add zsh python nut git vim tailscale
+
+# for unifi
+pkg_add unzip mongodb jdk javaPathHelper
 
 rcctl set syslogd flags "-U 127.0.0.1"
 rcctl restart syslogd
@@ -26,6 +24,18 @@ rcctl set ntpd flags -s
 rcctl enable upsmon
 
 rcctl enable tailscaled
+
+cd /tmp
+ftp https://cdn.openbsd.org/pub/OpenBSD/$(uname -r)/{ports.tar.gz,SHA256.sig}
+signify -Cp /etc/signify/openbsd-$(uname -r | cut -c 1,3)-base.pub -x SHA256.sig ports.tar.gz
+
+cd /usr
+tar xzf /tmp/ports.tar.gz
+
+cd /usr/ports/net/unifi/main
+make install
+
+rcctl enable unifi
 
 echo "Don't forget to login and run: "
 echo "   doas tailscale up --advertise-exit-node --advertise-routes=192.168.8.0/24 --accept-routes --ssh --accept-dns=false"
